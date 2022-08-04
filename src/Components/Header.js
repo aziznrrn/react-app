b78d4d98abc27e873d33f9af189462775228f71e
aziz <azizrn38@gmail.com>
2022-08-05 04:19:10 +0700

    add container to Navigation & header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,9 @@
 function Header() {
   return (
     <header className="border-b p-3 flex justify-between items-center">
-      <span className="font-bold">React App</span>
+      <div className="md:container md:mx-auto">
+        <span className="font-bold">React App</span>
+      </div>
     </header>
   )
 }
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -9,32 +9,34 @@ import {
 function Navigation() {
   return (
     <nav className="bg-gray-200 text-center p-2 absolute bottom-0 w-full border-t">
-      <ul className="flex flex-row justify-betweeen w-full">
-        <li className="items-center p-3">
-          <a href="/" className="text-gray-600">
-            <FontAwesomeIcon icon={faHome} className="text-gray-600" />
-            <span className="mx-1">Home</span>
-          </a>
-        </li>
-        <li className="items-center p-3">
-          <a href="contact" className="text-gray-600">
-            <FontAwesomeIcon icon={faAddressBook} className="text-gray-600" />
-            <span className="mx-1">Contact</span>
-          </a>
-        </li>
-        <li className="items-center p-3">
-          <a href="faq" className="text-gray-600">
-            <FontAwesomeIcon icon={faQuestionCircle} className="text-gray-600" />
-            <span className="mx-1">FAQ</span>
-          </a>
-        </li>
-        <li className="flex items-center p-3">
-          <a href="setting" className="text-gray-600">
-            <FontAwesomeIcon icon={faGear} className="text-gray-600" />
-            <span className="mx-1">Setting</span>
-          </a>
-        </li>
-      </ul>
+      <div className="md:container md:mx-auto"> 
+        <ul className="flex flex-row justify-between w-full">
+          <li className="items-center p-3">
+            <a href="/" className="text-gray-600">
+              <FontAwesomeIcon icon={faHome} className="text-gray-600" />
+              <span className="mx-1">Home</span>
+            </a>
+          </li>
+          <li className="items-center p-3">
+            <a href="contact" className="text-gray-600">
+              <FontAwesomeIcon icon={faAddressBook} className="text-gray-600" />
+              <span className="mx-1">Contact</span>
+            </a>
+          </li>
+          <li className="items-center p-3">
+            <a href="faq" className="text-gray-600">
+              <FontAwesomeIcon icon={faQuestionCircle} className="text-gray-600" />
+              <span className="mx-1">FAQ</span>
+            </a>
+          </li>
+          <li className="flex items-center p-3">
+            <a href="setting" className="text-gray-600">
+              <FontAwesomeIcon icon={faGear} className="text-gray-600" />
+              <span className="mx-1">Setting</span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
